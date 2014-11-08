f8917714efab39b369416a619b6b56f7e68b2b66
autopear <Merlin@Merlins-MacBook-Pro.local>
2014-11-08 18:47:25 +0800

    Update for iOS 8
    Also remove The Weather Channel logo

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 export ARCHS = arm64 armv7s armv7
-export TARGET = iphone:7.0:7.0
+export TARGET = iphone:8.1:7.0
 
 include theos/makefiles/common.mk
 
@@ -13,6 +13,6 @@ before-package::
 	chmod 0644 _/Library/MobileSubstrate/DynamicLibraries/NoYahoo.plist
 	plutil -convert binary1 _/Library/MobileSubstrate/DynamicLibraries/NoYahoo.plist
 	find _ -exec touch -r _/Library/MobileSubstrate/DynamicLibraries/NoYahoo.dylib {} \;
-        
+
 after-package::
 	rm -fr .theos/packages/*
--- Tweak.mm
+++ Tweak.mm
@@ -3,26 +3,36 @@
 
 #define YAHOO_ID @"com.apple.attributionweeapp.bundle"
 
-@interface WAWeatherCollectionFooterViewCell {
-	UIButton *_theWeatherChannelButton;
-	UIButton *_yahooButton;
-}
-@property(retain, nonatomic) UIButton *theWeatherChannelButton;
-@property(retain, nonatomic) UIButton *yahooButton;
-- (void)_yahooLogoTapped:(id)tapped;
-- (void)_twcLogoTapped:(id)tapped;
-- (WAWeatherCollectionFooterViewCell *)initWithFrame:(CGRect)frame;
-@end
-
 @interface WATouchButton : UIButton
 @end
 
-@interface TabController {
-	WATouchButton *_yahooButton;
+@interface WAWeatherCollectionFooterViewCell {
+	UIButton *_theWeatherChannelButton; //iOS 7 & 8
+	UIButton *_yahooButton; //iOS 7
+    WATouchButton* _addButton; //iOS 8
 }
-@property(retain, nonatomic) WATouchButton *yahooButton;
-- (void)yahooButtonPressed;
-- (TabController *)initWithFrame:(CGRect)frame withScrollIndicator:(BOOL)scrollIndicator;
+@property(retain, nonatomic) UIButton *theWeatherChannelButton; //iOS 7 & 8
+@property(retain, nonatomic) UIButton *yahooButton; //iOS 7
+@property(retain, nonatomic) WATouchButton* addButton; //iOS 8
+- (void)_yahooLogoTapped:(id)tapped; //iOS 7
+- (void)_twcLogoTapped:(id)tapped; //iOS 7 & 8
+- (WAWeatherCollectionFooterViewCell *)initWithFrame:(CGRect)frame;
+@end
+
+@interface TabController {
+	WATouchButton *_yahooButton; //iOS 7
+    WATouchButton* _infoButton; //iOS 8
+    WATouchButton* _twcButton; //iOS 8
+}
+@property(retain, nonatomic) WATouchButton *yahooButton; //iOS 7
+@property(retain, nonatomic) WATouchButton* twcButton; //iOS 8
+@property(retain, nonatomic) WATouchButton* infoButton; //iOS 8
+- (void)yahooButtonPressed; //iOS 7
+- (TabController *)initWithFrame:(CGRect)frame withScrollIndicator:(BOOL)scrollIndicator; //iOS 7
+- (TabController *)init; //iOS 8
+- (void)twcButtonPressed; //iOS 8
+- (void)infoButtonPressed; //iOS 8
+- (void)loadView; //iOS 8
 @end
 
 @interface StocksBacksideView : UIView {
@@ -36,14 +46,14 @@
 	UIButton *_infoButton;
 	UIButton *_viewStockButton;
 }
-- (StocksStatusView *)initWithStocksView:(id)stocksView;
+- (StocksStatusView *)initWithStocksView:(id)stocksView; //iOS 7
 - (void)_viewStockButtonPressed;
 - (void)_infoButtonPressed;
+- (StocksStatusView *)initWithFrame:(CGRect)frame; //iOS 8
 @end
 
-
 //Notfication Center
-%hook SBWidgetViewControllerHost
+%hook SBWidgetViewControllerHost //iOS 7 only
 
 + (BOOL)canLoadWidgetWithIdentifier:(NSString *)identifier forWidgetIdiom:(int)widgetIdiom bundlePath:(NSString *)path {
     if ([identifier isEqualToString:YAHOO_ID])
@@ -68,6 +78,14 @@
 
 %end
 
+%hook SBAttributionWrapperViewController //iOS 8 only
+
++ (SBAttributionWrapperViewController *)_newAttributionViewController {
+    return nil;
+}
+
+%end
+
 //Weather app
 %hook WAWeatherCollectionFooterViewCell
 
@@ -75,16 +93,17 @@
     WAWeatherCollectionFooterViewCell *cell = %orig(frame);
 
     cell.theWeatherChannelButton.hidden = YES;
-    cell.yahooButton.hidden = YES;
-    
+    if (kCFCoreFoundationVersionNumber < 1140.10)
+        cell.yahooButton.hidden = YES;
+
     return cell;
 }
 
--(void)_yahooLogoTapped:(id)tapped {
+- (void)_yahooLogoTapped:(id)tapped {
     return;
 }
 
--(void)_twcLogoTapped:(id)tapped {
+- (void)_twcLogoTapped:(id)tapped {
     return;
 }
 
@@ -95,10 +114,11 @@
 - (TabController *)initWithFrame:(CGRect)frame withScrollIndicator:(BOOL)scrollIndicator {
     TabController *ctrl = %orig(frame, scrollIndicator);
 
-    NSString *bundleIdentifier = [NSBundle mainBundle].bundleIdentifier;
-    
-    if (bundleIdentifier && [bundleIdentifier isEqualToString:@"com.apple.weather"]) {
-        ctrl.yahooButton.hidden = YES;
+    if (kCFCoreFoundationVersionNumber < 1140.10) {
+        NSString *bundleIdentifier = [NSBundle mainBundle].bundleIdentifier;
+
+        if (bundleIdentifier && [bundleIdentifier isEqualToString:@"com.apple.weather"])
+            ctrl.yahooButton.hidden = YES;
     }
 
     return ctrl;
@@ -106,12 +126,27 @@
 
 - (void)yahooButtonPressed {
     NSString *bundleIdentifier = [NSBundle mainBundle].bundleIdentifier;
-    
-    if (bundleIdentifier && [bundleIdentifier isEqualToString:@"com.apple.weather"]) {
+
+    if (bundleIdentifier && [bundleIdentifier isEqualToString:@"com.apple.weather"])
         return;
-    }
 
     %orig;
+}
+
+- (void)loadView {
+    %orig;
+
+    if (kCFCoreFoundationVersionNumber >= 1140.10) {
+        NSString *bundleIdentifier = [NSBundle mainBundle].bundleIdentifier;
+        if (bundleIdentifier && [bundleIdentifier isEqualToString:@"com.apple.weather"]) {
+
+            UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(twcButtonPressed)];
+            [self.infoButton addGestureRecognizer:longPress];
+            [longPress release];
+
+            self.twcButton.hidden = YES;
+        }
+    }
 }
 
 %end
@@ -122,10 +157,10 @@
 
 - (StocksBacksideView *)initWithFrame:(CGRect)frame {
     StocksBacksideView *view = %orig(frame);
-    
+
     UIButton *logoButton = CHIvar(view, _logoView, UIButton *);
     logoButton.hidden = YES;
-    
+
     return view;
 }
 
@@ -140,14 +175,34 @@
 - (StocksStatusView *)initWithStocksView:(id)stocksView {
     StocksStatusView *view = %orig(stocksView);
 
-    UIButton *infoButton = CHIvar(view, _infoButton, UIButton *);
-    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(_viewStockButtonPressed)];
-    [infoButton addGestureRecognizer:longPress];
-    [longPress release];
-    
-    UIButton *viewButton = CHIvar(view, _viewStockButton, UIButton *);
-    viewButton.hidden = YES;
-    
+    if (kCFCoreFoundationVersionNumber < 1140.10) {
+        //iOS 7
+        UIButton *infoButton = CHIvar(view, _infoButton, UIButton *);
+        UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:view action:@selector(_viewStockButtonPressed)];
+        [infoButton addGestureRecognizer:longPress];
+        [longPress release];
+
+        UIButton *viewButton = CHIvar(view, _viewStockButton, UIButton *);
+        viewButton.hidden = YES;
+    }
+
+    return view;
+}
+
+- (StocksStatusView *)initWithFrame:(CGRect)frame {
+    StocksStatusView *view = %orig(frame);
+
+    if (kCFCoreFoundationVersionNumber >= 1140.10) {
+        //iOS 8
+        UIButton *infoButton = CHIvar(view, _infoButton, UIButton *);
+        UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:view action:@selector(_viewStockButtonPressed)];
+        [infoButton addGestureRecognizer:longPress];
+        [longPress release];
+
+        UIButton *viewButton = CHIvar(view, _viewStockButton, UIButton *);
+        viewButton.hidden = YES;
+    }
+
     return view;
 }
 
